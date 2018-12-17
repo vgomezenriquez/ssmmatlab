c51d704ecb2b33320002cc965d8f9fef2c6d2be8
vgomezenriquez <45390554+vgomezenriquez@users.noreply.github.com>
2018-12-17 12:55:41 +0100

    Update tfsimul_d.m

--- SSM_tfsimul_d/tfsimul_d.m
+++ SSM_tfsimul_d/tfsimul_d.m
@@ -2,59 +2,60 @@
 %The input series is assumed to follow an ARIMA model. The model is
 %
 %  y_t =            u_t           +               v_t
-%      =  (3. + 1.5B)/(1-.5B)x_t  + (1-.4B)(1-.6B^12)/(1-B)(1-B^12)a_t
+%      =   (3.0B - 2.0B^2)X_t     +        (1 - 0.7B)/(1-B)A_t
 %
 % where x_t follows the model
 %
-% x_t = (1 - .7B^12)/(1-.5B)(1-B^12)b_t
+% x_t = 1/(1-B)b_t
 %
-% and std(a_t) = 1. and std(b_t)=.5.
+% and std(a_t) = 1. and std(b_t)=sqrt(.5).
 %
 
 clear
 
-freq = 12;
+freq = 1;
 
-x = arimasimeasy(freq, '[p dr q]', [1, 0, 0], '[ps ds qs]', [0, 1, 1], ...
-    'phir', [-.5, 1], 'ths', [-.7, 1], 'N', 150, 'discard', ...
-    50, 'seed', 18, 'stda', .5, 'gft', 0);
+x = arimasimeasy(freq, '[p dr q]', [0, 1, 0], 'N', 150, 'discard', ...
+    50, 'seed', 18, 'stda', sqrt(.5), 'gft', 0);
 
 
-v = arimasimeasy(freq, '[p dr q]', [0, 1, 1], '[ps ds qs]', [0, 1, 1], ...
-    'thr', [-.4, 1], 'ths', [-.6, 1], 'N', 150, 'discard', ...
+v = arimasimeasy(freq, '[p dr q]', [0, 1, 1], 'thr', [-.7, 1], 'N', 150, 'discard', ...
     50, 'seed', 20, 'stda', 1., 'gft', 0);
 
 %third, filter inputs by Phi(B)^{-1}Gamma(B)
 %1) without the input model
-thp = [1.5, 3.];
-phip = [-.5, 1.];
-% u01 = varmafilp(x,phip,thp);
-%
-% omega(:,:,1)=3.; omega(:,:,2)=1.5;
-% delta(:,:,1)=1.; delta(:,:,2)=-.5;
-% u02 = varmafilp(x,delta,omega);
+thp = [-2., 3., 0.];
+phip = 1.;
+
+u01 = varmafilp(x, phip, thp);
+
+omega(:, :, 1) = 0.;
+omega(:, :, 2) = 3.;
+omega(:, :, 3) = -2.;
+delta(:, :, 1) = 1.;
+u02 = varmafilp(x, delta, omega);
 
 %2) with the input model
 %set up state space form for input model
-phix = [-.5, 1.];
-Phix = [-1., 1.];
-Thx = [-.7, 1.];
+phix = [-1., 1.];
+Phix = 1.;
+Thx = 1.;
 thx = 1.;
-Sigma = .5^2;
+Sigma = .5;
 u1 = varmafilp(x, phip, thp, phix, thx, Phix, Thx, Sigma, freq);
 
-%  phi(:,:,1)=1; phi(:,:,2)=-.5;
-%  Phi(:,:,1)=1; Phi(:,:,2)=-1.;
-%  Th(:,:,1)=1.; Th(:,:,2)=-.7;
-%  th(:,:,1)=1.;
-%  u2 = varmafilp(x,delta,omega,phi,th,Phi,Th,Sigma,freq);
+phi(:, :, 1) = 1;
+phi(:, :, 2) = -1.;
+Phi(:, :, 1) = 1;
+Th(:, :, 1) = 1.;
+th(:, :, 1) = 1.;
+u2 = varmafilp(x, delta, omega, phi, th, Phi, Th, Sigma, freq);
 
-u = u1;
+u = u2;
 
 y = v + u;
 
-% y(5:9)=NaN(5,1);
-% y(30)=NaN;
+% y(6:10)=NaN(5,1);     we add some missing values
 
 % out=arimaeasy(x,freq,'pr',1,'gft',1,'sname','myseries');
 % out=arimaeasy(v,freq,'pr',1,'gft',1,'sname','myseries');
