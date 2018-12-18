32d4212e9d98cddc79f65451cbe4693c49630014
vgomezenriquez <45390554+vgomezenriquez@users.noreply.github.com>
2018-12-18 17:54:34 +0100

    Add files via upload

---
+++ SSM_arimasimul2_d/arimasimul2_d.m
@@ -0,0 +1,14 @@
+% script file to simulate a series that follows an ARMA(2,1) series.
+% Example in Section 2.1.1 of the book 'Linear Time Series With MATLAB and
+% OCTAVE'
+%
+clear
+
+freq = 1;
+phir = [-.8, 1];
+phir = conv(phir, [-.7, 1]);
+thr = [-.4, 1];
+
+y = arimasimeasy(freq, '[p dr q]', [2, 0, 1], '[ps ds qs]', [0, 0, 0], ...
+    'phir', phir, 'thr', thr, 'N', 100, 'discard', ...
+    50, 'seed', 20, 'gft', 1);
